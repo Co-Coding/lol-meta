31d36d961bda838c0fbf0fc96b7a0285a08de2da
CArlos <cgfigueroa95@gmail.com>
2020-03-25 21:12:32 -0300

    design improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,7 +24,8 @@ import { DomseguroPipe } from './pipes/domseguro.pipe';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
     
   ],
   providers: [],
